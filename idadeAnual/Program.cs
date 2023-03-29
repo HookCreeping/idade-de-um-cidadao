1c6e9f8816a2fcd46b8708cfac89b41366eb52de
Henrique Baptista De Oliveira <54454818835@info.local>
2023-03-29 17:10:10 -0300

    update1

---
+++ idadeAnual/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Insira a sua idade: ");
+float IdadeAnos = float.Parse(Console.ReadLine());
+
+float IdadeMeses = IdadeAnos * 12;
+float IdadeDias = IdadeAnos * 365;
+float IdadeHoras = IdadeDias * 24;
+float IdadeMinutos = IdadeHoras * 60;
+float IdadeSegundos = IdadeMinutos * 60;
+
+Console.WriteLine($"Você tem: {IdadeAnos} anos, que é igual a {IdadeMeses} meses, equivalente a {IdadeDias} dias, que se iguala a {IdadeHoras} horas, que se resulta em {IdadeMinutos} minutos, assim, sua idade em segundos é {IdadeSegundos}."
+);
+
+
+
---
+++ idadeExataAtual/Program.cs
@@ -0,0 +1,9 @@
+﻿// Descubrir a idade de alguém em anos e semanas através do seu aniversário
+
+Console.WriteLine($"Diga em que ano você nasceu: "); float AnoNascimento = float.Parse(Console.ReadLine());
+var AnoAtual = DateTime.Now.Year;
+
+float IdadeAnos = AnoAtual - AnoNascimento;
+float IdadeSemanas = (IdadeAnos * 12) * 4;
+
+Console.WriteLine($"Você tem {IdadeAnos} anos, equivalente a {IdadeSemanas} semanas");
